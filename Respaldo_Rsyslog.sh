f8902f2186aec983867e0406ace252d202e37ebe
Pablo <juanpa@linuxpro.org>
2020-05-29 15:36:55 -0500

    Se agrega la seccion para respaldar los archivos y la db en un tar.gz

--- Respaldo_Rsyslog.sh
+++ Respaldo_Rsyslog.sh
@@ -10,3 +10,14 @@ db=tu_base_de_datos
 mysqldump -u $user -p$pass $db > Respaldo"_"$fecha.sql
 sleep 10
 
+#Manejo de archivos
+mkdir Respaldos_$fecha
+cp -R /etc/rsyslog.conf Respaldos_$fecha/
+cp -R /var/www/html/Syslog/ Respaldos_$fecha/
+cp -R /etc/rsyslog.d/ Respaldos_$fecha/
+mv *.sql Respaldos"_"$fecha
+sleep 10
+
+#Se crea un tar.gz con la carpeta que contiene todos los archivos
+tar -czvf Respaldos_Syslog_$fecha.tar.gz Respaldos_$fecha/
+sleep  10
